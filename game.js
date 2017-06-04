fedcfaf053614487606e1b5cbf5eb428ca6b670c
polgasull <pol89.gn@gmail.com>
2017-06-04 22:10:49 +0200

    more

--- game.js
+++ game.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+$(".top-target-img").click(function() {
+  $(".top-target-img").hide();
+});
+
+$(".mid-target-img").click(function() {
+  $(".mid-target-img").hide();
+});
+
+$(".bottom-target-img").click(function() {
+  $(".bottom-target-img").hide();
+});
+
 
 
 });
