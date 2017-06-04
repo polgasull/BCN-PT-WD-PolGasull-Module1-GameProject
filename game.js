cf4ae0fd4240b949fdf8619348848571a4ce4dc5
polgasull <pol89.gn@gmail.com>
2017-06-04 22:03:15 +0200

    empezando de nuevo

--- game.js
+++ game.js
@@ -1,51 +1,5 @@
 $(document).ready(function() {
 
-  function Game(options) {
-      this.rows    = options.rows;
-      this.columns = options.columns;
-      this.target   = {
-        row: Math.floor(Math.random() * this.rows),
-        column: Math.floor(Math.random() * this.columns)
-    };
-
-      for (var rowIndex = 0; rowIndex < options.rows; rowIndex++){
-         for (var columnIndex = 0; columnIndex < options.columns; columnIndex++){
-           $('.container').append($('<div>')
-             .addClass('cell board')
-             .attr('data-row', rowIndex)
-             .attr('data-col', columnIndex)
-
-           );
-         }
-       }
-      }
-
-      Game.prototype.generateTarget = function() {
-        this.target = {
-          row: Math.floor(Math.random() * this.rows),
-          column: Math.floor(Math.random() * this.columns)
-        };
-      };
-
-      Game.prototype.drawTarget = function(){
-        var selector = '[data-row=' + this.target.row + '][data-col=' + this.target.column + ']';
-        $(selector).addClass('target');
-      };
-
-      Game.prototype.killTarget = function() {
-        $(".target").click(function(){
-          $(".target").hide();
-      });
-    };
-
-var game = new Game({
-  rows: 55,
-  columns: 60,
-});
-
-game.generateTarget();
-game.drawTarget();
-game.killTarget();
 
 
 });
