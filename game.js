524937458e76876904d87e4c4e1ad2138cc2bee1
polgasull <pol89.gn@gmail.com>
2017-06-06 21:33:11 +0200

    lol

--- game.js
+++ game.js
@@ -1,7 +1,51 @@
 $(document).ready(function() {
 
+  function Game(options) {
+      this.rows    = options.rows;
+      this.columns = options.columns;
+      this.target = {
+      row: Math.floor(Math.random() * this.rows),
+      column: Math.floor(Math.random() * this.columns)
+      };
+
+  for (var rowIndex = 0; rowIndex < this.rows; rowIndex++){
+     for (var columnIndex = 0; columnIndex < this.columns; columnIndex++){
+       $('.container').append($('<div>')
+         .addClass('cell board')
+         .attr('data-row', rowIndex)
+         .attr('data-col', columnIndex)
+       );
+     }
+   }
+  }
+
+  Game.prototype.generateTarget = function() {
+    while (this.target) {
+      this.food = {
+        row: Math.floor(Math.random() * this.rows),
+        column: Math.floor(Math.random() * this.columns)
+      };
+    }
+  };
+
+  Game.prototype.drawTarget = function(){
+    var selector = '[data-row=' + this.target.row + '][data-col=' + this.target.column + ']';
+    $(selector).addClass("target");
+  };
+
+  Game.prototype.clearTarget = function() {
+    $(".Target").removeClass("target");
+  };
+
+
+  var game = new Game({
+    rows: 50,
+    columns: 52,
+  });
+
 
 });
+
 
 
 
--- target.js
+++ target.js
@@ -1,37 +1,3 @@
-$(document).ready(function() {
-
-// target
-var target = $(".target");
-var targetLoad = (target).hide(0);
-var posx = (Math.floor(Math.random() * ($(".board").width() )));
-var posy = (Math.floor(Math.random() * ($(".board").height() )));
-
 function Target() {
-  this.position = 1;
 
-  this.targetShow = setTimeout(function(){
-   (target).show();
- }, 3000);
- this.targetHide = setTimeout(function() {
-   (target).hide();
- }, 6000);
-  this.dead = (target).click(function() {
-    (target).css('background', 'yellow');
-    (target).hide(2000);
-  });
-  this.randomTarget = function() {
-    $(target).css({
-      'position':'relative',
-      'left':posx+'px',
-      'top':posy+'px',
-      'display':'inline-flex'
-      }).fadeIn(1000).delay(1000).fadeOut(500, function(){
-        $(this).remove();
-      });
-    }();
-  }
-
-  Target();
-
-
-});
+}
