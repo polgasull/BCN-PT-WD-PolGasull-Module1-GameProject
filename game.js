ca0155b797dd470a59b2bf251fbbff20d9adda7a
polgasull <pol89.gn@gmail.com>
2017-06-12 23:32:12 +0200

    trying to pause game

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
-
 function Game(options) {
       this.rows    = options.rows;
       this.columns = options.columns;
       this.target = new Target(options.rows, options.columns);
       this.points = 0;
-      this.timer = 10;
+      this.timer = 25;
 
   for (var rowIndex = 0; rowIndex < this.rows; rowIndex++){
      for (var columnIndex = 0; columnIndex < this.columns; columnIndex++){
@@ -17,81 +16,101 @@ function Game(options) {
    }
   }
 
-  Game.prototype.generateTarget = function() {
-    var targetType = Math.random() < 0.7;
-    this.target = new Target(this.rows, this.columns, targetType);
-  };
+Game.prototype.generateTarget = function() {
+  var targetType = Math.random() < 0.7;
+  this.target = new Target(this.rows, this.columns, targetType);
+};
 
-  Game.prototype.start = function() {
-    var self = this;
-    this.countdown = setInterval( function() {
-      self.countDownTimer();
-    }, 1000);
-      this.generateTarget();
-
-    this.autoClear = setInterval( function(){
-        self.autoRemoveTarget();
-      }, 3000);
-  };
-
-  Game.prototype.drawTarget = function(){
-    var selector = '[data-row=' + this.target.row + '][data-col=' + this.target.column + ']';
-    $(selector).addClass('target');
-
-    if (this.target.targetRole){
-      $(selector).addClass('terro');
-    } else {
-      $(selector).addClass('pollo');
-    }
-
-    this.touchTarget();
-  };
-
-  Game.prototype.autoRemoveTarget = function() {
-    $("div").unbind();
-    this.removeAllTargets();
+Game.prototype.start = function() {
+  var self = this;
+  this.countdown = setInterval( function() {
+    self.countDownTimer();
+  }, 1000);
     this.generateTarget();
-    this.drawTarget();
-  };
 
-  Game.prototype.touchTarget = function() {
-    var self = this;
-    $('.target').on("click",function(){
-      var targetTouched = self.target.isTouched();
-        if (targetTouched) {
-          $(".target").addClass("deadTarget");
-          self.countPoints();
-          $("div").unbind();
-          setTimeout(function(){
-            $(".deadTarget").removeClass("deadTarget");
-            self.autoRemoveTarget();
-          }, 2000);
-        }
-    });
-  };
+  this.autoClear = setInterval( function(){
+      self.autoCreateTarget();
+    }, 3000);
+};
 
-  Game.prototype.removeAllTargets = function() {
-    $(".target").removeClass("target pollo terro");
-  };
+Game.prototype.drawTarget = function(){
+  var selector = '[data-row=' + this.target.row + '][data-col=' + this.target.column + ']';
+  $(selector).addClass('target');
 
-  Game.prototype.countPoints = function() {
-    console.log(this.points);
-    if (this.target.targetRole) {
-      $(".totalPoints span").text(this.points += 5);
-    } else {
-      $(".totalPoints span").text(this.points -= 5);
-    }
-  };
+  if (this.target.targetToKill){
+    $(selector).addClass('terro');
+  } else {
+    $(selector).addClass('pollo');
+  }
 
-  Game.prototype.countDownTimer = function() {
-      $(".displayTimer span").text(this.timer--);
-      if (this.timer === -1) {
-        $( ".container" ).append( "<p>GAME OVER</p>" );
-        clearInterval(this.countdown);
-        this.removeAllTargets();
-        clearInterval(this.autoClear);
+  this.touchTarget();
+};
+
+Game.prototype.autoCreateTarget = function() {
+  $("div").unbind();
+  this.removeAllTargets();
+  this.generateTarget();
+  this.drawTarget();
+};
+
+Game.prototype.touchTarget = function() {
+  var self = this;
+  $('.target').on("click",function(){
+    var targetTouched = self.target.isTouched();
+      if (targetTouched) {
+        $(".target").addClass("deadTarget");
+        self.countPoints();
+        $("div").unbind();
+        setTimeout(function(){
+          $(".deadTarget").removeClass("deadTarget");
+          self.autoCreateTarget();
+        }, 2000);
       }
-  };
+  });
+};
+
+Game.prototype.removeAllTargets = function() {
+  $(".target").removeClass("target pollo terro");
+};
+
+Game.prototype.countPoints = function() {
+  console.log(this.points);
+  if (this.target.targetToKill) {
+    $(".totalPoints span").text(this.points += 5);
+  } else {
+    $(".totalPoints span").text(this.points -= 5);
+  }
+};
+
+Game.prototype.countDownTimer = function() {
+    $(".displayTimer span").text(this.timer--);
+    if (this.timer === -1) {
+      $( ".container" ).append( "<p>GAME OVER</p>" );
+      clearInterval(this.countdown);
+      this.removeAllTargets();
+      clearInterval(this.autoClear);
+    }
+};
+
+Game.prototype.assignControlsToKeys = function() {
+  $("body").on("keydown", function(event){
+    switch (event.keyCode) {
+      case 80:
+        if (this.countdown) {
+          this.stop();
+        } else {
+          this.start();
+        }
+    }
+  }.bind(this));
+};
+
+Game.prototype.stop = function() {
+  if (this.countdown) {
+    clearInterval(this.countdown);
+  }
+};
+
 
 $(document).ready(function() {
 
@@ -101,6 +120,7 @@ $(document).ready(function() {
   });
 
   myGame.start();
+  game.assignControlsToKeys();
 
 });
 
--- target.js
+++ target.js
@@ -3,7 +3,7 @@ function Target(gameRows, gameColumns, targetRole) {
   this.row = Math.floor(Math.random() * gameRows);
   this.column = Math.floor(Math.random() * gameColumns);
   this.touched = false;
-  this.targetRole = targetRole;
+  this.targetToKill = targetRole;
 
 }
 
